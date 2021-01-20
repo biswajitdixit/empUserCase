c886982d8477afb7e28ce05aaf52f6b42d5b55b5
biswajitdixit <buntyoec1998@gmail.com>
2021-01-20 18:08:21 +0530

    ADDED:The emp wage cal

--- EmpWage.java
+++ EmpWage.java
@@ -1,11 +1,16 @@
 public class EmpWage{
 static final int IS_FULL_TIME =1;
+static final int EMP_RATE_PER_HRS=20;
 public static void main(String[]args){
  System.out.println("Welcome to emp wage computation ");
+ int empHrs = 0;
+ int empWage = 0;
  double empCheck =Math.floor(Math.random()* 10) % 2;
  if (empCheck == IS_FULL_TIME)
-    System.out.println("Employee is Present");
+    empHrs=8;
  else
-    System.out.println("Emplyee is Abscent");
+    empHrs=0;
+ empWage=empHrs * EMP_RATE_PER_HRS;
+ System.out.println("Emp Wage: " + empWage );
  }
 }
