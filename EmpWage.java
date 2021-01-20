780c4ffc0a9a2d5ecea2b1de5349a530e7c6b5a9
biswajitdixit <buntyoec1998@gmail.com>
2021-01-20 18:03:49 +0530

    UPDATED:The emp attendance

--- EmpWage.java
+++ EmpWage.java
@@ -1,5 +1,11 @@
 public class EmpWage{
+static final int IS_FULL_TIME =1;
 public static void main(String[]args){
  System.out.println("Welcome to emp wage computation ");
+ double empCheck =Math.floor(Math.random()* 10) % 2;
+ if (empCheck == IS_FULL_TIME)
+    System.out.println("Employee is Present");
+ else
+    System.out.println("Emplyee is Abscent");
  }
 }
